891b8c9db97c905b2ca99e64fea7aaf0a6a8bc99
ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>
2026-07-11 21:39:31 +0530

    Delete Profile.js
    Signed-off-by: ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>

--- Profile.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("saveBtn").addEventListener("click", () => {
-    alert("Profile saved!");
-});
-
-document.getElementById("logoutBtn").addEventListener("click", () => {
-    alert("Logged out!");
-    window.location.href = "login.html";
-});
