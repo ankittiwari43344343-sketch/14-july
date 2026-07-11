714b9712a93d7788991666cf79e41812b00fb832
ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>
2026-07-11 11:36:22 +0530

    Create firebase.js
    Signed-off-by: ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkMzgmF8NRPdX0weTJ1yqo2HIUSil2OQ0",
+  authDomain: "july-f8b7b.firebaseapp.com",
+  projectId: "july-f8b7b",
+  storageBucket: "july-f8b7b.firebasestorage.app",
+  messagingSenderId: "680464782562",
+  appId: "1:680464782562:web:e35746ae4a958d58be7abb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
