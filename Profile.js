11fa16a110614278dd8bfa63cb11a92cb6abaec3
ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>
2026-07-11 21:08:53 +0530

    Create Profile.js
    Signed-off-by: ankittiwari43344343-sketch <ankittiwari43344343@gmail.com>

---
+++ Profile.js
@@ -0,0 +1,7 @@
+document.getElementById("saveBtn").addEventListener("click", () => {
+    alert("Profile saved!");
+});
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    alert("Logged out!");
+    window.location.href = "profile.html";
